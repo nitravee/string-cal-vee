14270ed089f7155303d4d842dcbbebf317d09fb3
Nitravee Wongvitavas <nitravee@Nitravees-MacBook-Pro.local>
2018-08-16 16:51:00 +0700

    add empty string function

--- test/test-string-calculator.js
+++ test/test-string-calculator.js
@@ -1,4 +1,7 @@
+
+
 const chai = require('chai');
+const emptyStr = require('./test.js');
 
 const { expect } = chai;
 
@@ -12,12 +15,18 @@ function itAlwaysBe2() {
   expect(2).to.be.equal(2);
 }
 
+
+function emptyString() {
+  expect(emptyStr('')).to.be.equal(0);
+}
+
+
 // Start Example Behaviors
 function exampleBehaviors() {
   it('should be true', itAlwaysTrue);
   it('should be equal 2', itAlwaysBe2);
+  it('should be 0', emptyString);
 }
 
 // Start Describe
 describe('Example', exampleBehaviors);
-
---
+++ test/test.js
@@ -0,0 +1,9 @@
+
+function emptyString(a) {
+  if (a === '') {
+    return 0;
+  }
+  return 1;
+}
+
+module.exports = emptyString;
