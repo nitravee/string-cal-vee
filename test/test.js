ada8beeb4b9a602a7e02959f76ca1a195b64013f
wichittar ongsakpornnimit <wichittarongsakpornnimit@paimacpro.wifi.bkk.throughwave.com>
2018-08-17 16:18:15 +0700

    add ignor number >1000

--- test/test.js
+++ test/test.js
@@ -3,7 +3,17 @@ function emptyString(a) {
   if (a === '') {
     return 0;
   } else if (a.includes(',') || a.includes('\n')) {
-    return a.split(/[\s,]+/).map(Number).reduce((x, y) => x + y, 0);
+    a.split(/[\s,]+/).forEach((element) => {
+      if (parseInt(element, 10) < 0) {
+        throw new Error('Have Negative');
+      }
+    });
+    return a.split(/[\s,]+/).map(Number).reduce((x, y) => {
+      if (y <= 1000) {
+        return x + y;
+      }
+      return x;
+    }, 0);
   } else if (parseInt(a, 10)) {
     return parseInt(a, 10);
   }
